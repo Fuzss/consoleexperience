806ea48864977791e96abae99e54f712e7789cdd
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-09-28 21:36:55 +0200

    potion dot experiments

--- src/main/java/fuzs/consoleexperience/mixin/client/ItemRendererMixin.java
+++ src/main/java/fuzs/consoleexperience/mixin/client/ItemRendererMixin.java
@@ -10,8 +10,11 @@ import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PotionItem;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.resources.IResourceManagerReloadListener;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.TextFormatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,9 +26,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import javax.annotation.Nullable;
 import java.util.List;
 
-@SuppressWarnings({"deprecation", "unused"})
 @Mixin(ItemRenderer.class)
-public abstract class ItemRendererMixin implements IResourceManagerReloadListener {
+abstract class ItemRendererMixin implements IResourceManagerReloadListener {
 
     @Inject(method = "renderGuiItem(Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/renderer/model/IBakedModel;)V", at = @At(value = "NEW", args = "class=com/mojang/blaze3d/matrix/MatrixStack"))
     protected void renderGuiItem(ItemStack p_191962_1_, int p_191962_2_, int p_191962_3_, IBakedModel p_191962_4_, CallbackInfo callbackInfo) {
@@ -37,9 +39,10 @@ public abstract class ItemRendererMixin implements IResourceManagerReloadListene
     @Inject(method = "renderGuiItemDecorations(Lnet/minecraft/client/gui/FontRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("HEAD"))
     public void renderGuiItemDecorations(FontRenderer fr, ItemStack stack, int xPosition, int yPosition, @Nullable String text, CallbackInfo callbackInfo) {
 
-        if (!stack.isEmpty() && stack.getItem() instanceof PotionItem) {
+        if (stack.getItem() instanceof PotionItem) {
 
-            final int dotCount = getDotCount(PotionUtils.getMobEffects(stack));
+            List<EffectInstance> mobEffects = PotionUtils.getMobEffects(stack);
+            int dotCount = getDotCount(mobEffects);
             if (dotCount > 0) {
 
                 RenderSystem.disableDepthTest();
@@ -50,10 +53,33 @@ public abstract class ItemRendererMixin implements IResourceManagerReloadListene
                 BufferBuilder bufferbuilder = tessellator.getBuilder();
 
                 this.fillRect(bufferbuilder, xPosition + 3, yPosition + 13, 11, 2, 0, 0, 0, 255);
-                final int color = TextFormatting.AQUA.getColor();
-                for (int i = 0; i < dotCount; i++) {
+//                int color = TextFormatting.AQUA.getColor();
 
-                    this.fillRect(bufferbuilder, xPosition + 3 + 3 * i, yPosition + 13, 2, 2, color >> 16 & 255, color >> 8 & 255, color & 255, 255);
+                TextFormatting[] colors = new TextFormatting[]{TextFormatting.AQUA, TextFormatting.LIGHT_PURPLE, TextFormatting.GOLD, TextFormatting.GREEN, TextFormatting.YELLOW, TextFormatting.RED};
+
+//                int[] dotColors = new int[4];
+//                dotColors[0] = PotionUtils.getColor(stack);
+                Potion potion = PotionUtils.getPotion(stack);
+                ResourceLocation potionKey = Registry.POTION.getKey(potion);
+//                color = potionKey.getPath().startsWith("strong_") ? TextFormatting.GOLD.getColor() : (potionKey.getPath().startsWith("long_") ? TextFormatting.RED.getColor() : TextFormatting.AQUA.getColor());
+//                dotColors[2] = PotionUtils.getMobEffects(stack).stream().map(EffectInstance::getEffect).anyMatch(((Predicate<Effect>) Effect::isBeneficial).negate()) ? TextFormatting.RED.getColor() : TextFormatting.BLUE.getColor();
+//                dotColors[3] = IntStream.range(0, 16).mapToObj(i -> TextFormatting.values()[15 - i]).skip(dotCount % 16).findFirst().map(TextFormatting::getColor).orElse(TextFormatting.AQUA.getColor());
+
+//                dotCount += mobEffects.size() - 1;
+//                if (potionKey.getPath().startsWith("strong_")) {
+//                    dotCount += 4;
+//                }
+//                if (potionKey.getPath().startsWith("long_")) {
+//                    dotCount += 8;
+//                }
+
+                for (int i = 0; i < Math.min(4, dotCount); i++) {
+//                    if ((dotCount >> i & 1) == 1) {
+////                    int color = dotColors[i];
+//                    }
+
+                    int color = colors[Math.min((dotCount - i - 1) / 4, colors.length - 1)].getColor();
+                    this.fillRect(bufferbuilder, xPosition + 3 + 3 * i, yPosition + 13, 2, 2, color >> 16 & 0xFF, color >> 8 & 0xFF, color & 0xFF, 0xFF);
                 }
 
                 RenderSystem.enableBlend();
@@ -72,19 +98,7 @@ public abstract class ItemRendererMixin implements IResourceManagerReloadListene
 
     @Unique
     private int getDotCount(List<EffectInstance> list) {
-
-        if (list.isEmpty()) {
-
-            return 0;
-        }
-
-        int dotCount = 0;
-        for (EffectInstance effectInstance : list) {
-
-            dotCount += effectInstance.getAmplifier() + 1;
-        }
-
-        return Math.min(dotCount, 4);
+        return list.stream().mapToInt(EffectInstance::getAmplifier).map(i -> i + 1).sum();
     }
 
 }
